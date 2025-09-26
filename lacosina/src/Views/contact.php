2385831f5bab3a1b209db8e303fc6e712be674b9
toast <159030724+mgorce4@users.noreply.github.com>
2025-09-26 18:58:27 +0200

    ajouter formulaire de contact

--- lacosina/src/Views/contact.php
+++ lacosina/src/Views/contact.php
@@ -1,2 +1,18 @@
-<h1>Contact</h1>
-<p>Vous pouvez nous contacter à l’adresse suivante : contact@lacosina.fr</p>
+<h1>Formulaire de contact</h1>
+<form action="?c=enregistrer" method="post">
+    <div class="mb-3">
+        <label for= "nom" class="form-label">Votre nom</label>
+        <input type="text" class="form-control"  name="nom" id="nom" required>
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Votre email</label>
+        <textarea class="form-control" name="email" id="email" required></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea class="form-control" name="description" id="desription" rows=3 required></textarea>
+    </div>
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary" id="enregistrer" >Enregistrer</button>
+    </div>
+</form>
